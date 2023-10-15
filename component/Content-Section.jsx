6d84dec01cd0167e219b314ee78ab74a0963c953
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2023-10-15 12:51:30 +0700

    feat:add new resume

--- component/Content-Section.jsx
+++ component/Content-Section.jsx
@@ -1,13 +1,12 @@
-import React from "react";
 import "./ContentSection.css";
 
 function ContentSection() {
 
   const handleDownloadCV = () => {
-    const fileURL = '/Copy of Rachaphol_Plookaom_Resume.pdf';
+    const fileURL = '/Rachaphol-Resume.pdf';
     const downloadLink = document.createElement('a');
     downloadLink.href = fileURL;
-    downloadLink.download = 'your-file-resume.pdf'; // Set the desired file name for download
+    downloadLink.download = 'Rachaphol-Resume.pdf'; 
     document.body.appendChild(downloadLink)
     downloadLink.click();
     document.body.removeChild(downloadLink);
@@ -18,8 +17,8 @@ function ContentSection() {
       {/* Left Box */}
       <div className="contentLeftbox">
         <div>Hi!</div>
-          <div className="subtext">I'm  Rachaphol Plookaom </div>
-          Web Developer in Training
+          <div className="subtext"> I am  Rachaphol Plookaom </div>
+          Web Developer 
         <div className="maintext"> I am passionate about applying my analytical skills and <br></br>attention to detail to ensure the quality and functionality of software products. </div>
         <div>
           <div className="socialBox"><div className="socialText"> Follow me </div><div className="seperator"></div> <img src="/socialMedia.svg" alt="socialMedia" /> </div>
