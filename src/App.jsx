0189bcc69178f46f74e8f3eeba4c7026fa70e928
Rachaphol Plookaom <124332198+naiplawan@users.noreply.github.com>
2024-07-14 21:30:41 +0700

    Update import paths for pages in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "@/page/HomePage.jsx";
-import ErrorPage from "@/page/ErrorPage.jsx";
-import AboutPage from "@/page/AboutPage.jsx";
-import PortfolioPage from "@/page/PortfolioPage.jsx";
-import ContactPage from "@/page/ContactPage.jsx";
+import HomePage from "./page/HomePage.jsx";
+import ErrorPage from "./page/ErrorPage.jsx";
+import AboutPage from "./page/AboutPage.jsx";
+import PortfolioPage from "./page/PortfolioPage.jsx";
+import ContactPage from "./page/ContactPage.jsx";
 
 function App() {
   return (
